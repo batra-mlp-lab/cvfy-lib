b35e72ca2d3711a40ea7675759125b8e608094ee
Owen Leong <owenl131@gmail.com>
2017-12-04 08:54:46 +0800

    Move assertions from server to test file for all

--- origami-lib/test/server_textimageinput.py
+++ origami-lib/test/server_textimageinput.py
@@ -34,53 +34,36 @@ def test():
     # 3 for all
     tnum = origami.request.form['test_number']
 
-    if tnum == '0' or tnum == '3':
+    if tnum == '0':
         print('hello')
         # test getTextArray
         all_text = origami.getTextArray()
         print('Received %d text strings'%(len(all_text)))
         print(all_text)
-        # check that number of entries received is correct
-        if len(all_text) != len(text_list):
-            return 'Wrong number of strings received', 500
-        # check that strings received match expected strings
-        if all_text != text_list:
-            return 'Text data not received correctly', 500
-        if tnum == '0':
-            return jsonify({'data': all_text}), 200        
+        return jsonify({'data': all_text}), 200        
     
-    if tnum == '1' or tnum == '3':
+    elif tnum == '1':
         # test getImageArray with file_path mode
         all_image_paths = origami.getImageArray(mode='file_path')
         print('Received %d image paths'%(len(all_image_paths)))
         print(all_image_paths)
-        # check that number of entries received is correct
-        if len(all_image_paths) != len(image_list):
-            return 'Wrong number of images received', 500
-        # check that images are correctly decoded into numpy arrays
-        for i in range(len(all_image_paths)):
-            im1 = cv2.imread(all_image_paths[i])
-            im2 = cv2.imread(image_list[i])
-            # check that images match in shape and pixel values
-            if not np.array_equal(im1, im2):
-                return 'Images not saved correctly', 500
-        if tnum == '1':
-            return jsonify({'data': all_image_paths}), 200
+        return jsonify({'data': all_image_paths}), 200
     
-    if tnum == '2' or tnum == '3':
+    elif tnum == '2':
         # test getImageArray with numpy_array mode
         all_images = origami.getImageArray(mode='numpy_array')
-        # check that number of entries received is correct
-        if len(all_images) != len(image_list):
-            return 'Wrong number of images received', 500
-        # check that images are saved correctly into given filepaths
-        for i in range(len(all_images)):
-            im = cv2.imread(image_list[i])
-            # check that images match in shape and pixel values
-            if not np.array_equal(all_images[i], im):
-                return 'Images not converted correctly', 500
-        if tnum == '2':
-            return jsonify({'data': [a.tolist() for a in all_images]}), 200
+        return jsonify({'data': [a.tolist() for a in all_images]}), 200
+    
+    elif tnum == '3':
+        all_text = origami.getTextArray()
+        all_image_paths = origami.getImageArray(mode='file_path')
+        all_images = origami.getImageArray(mode='numpy_array')
+        return jsonify({'text': all_text, 
+                        'filepaths': all_image_paths, 
+                        'images': [a.tolist() for a in all_images]}), 200
+
+    else:
+        return 'Please define test number', 500
 
     return 'OK'
 
--- origami-lib/test/test_textimageinput.py
+++ origami-lib/test/test_textimageinput.py
@@ -23,22 +23,25 @@ def test_getTextInput():
     data_copy = data.copy()
     data_copy['test_number'] = 0
     r = requests.post(url=url, data=data_copy)
-    assert(r.status_code == 200)
-    assert(json.loads(r.content)['data'] == ['hello', 'world', 'Is this a sample question?'])
+    text_array = json.loads(r.content)['data']
+    assert(len(text_array) == 3)
+    assert(text_array == ['hello', 'world', 'Is this a sample question?'])
 
 def test_getWrongTextInput():
     data_copy = data.copy()
     data_copy['test_number'] = 0
     data_copy['input-text-0'] = 'goodbye'
     r = requests.post(url=url, data=data_copy)
-    assert(r.status_code == 500)
+    text_array = json.loads(r.content)['data']
+    assert(text_array != ['hello', 'world', 'Is this a sample question?'])
 
 def test_getExtraTextInput():
     data_copy = data.copy()
     data_copy['test_number'] = 0
     data_copy['input-text-3'] = 'goodbye'
     r = requests.post(url=url, data=data_copy)
-    assert(r.status_code == 500)
+    text_array = json.loads(r.content)['data']
+    assert(text_array != ['hello', 'world', 'Is this a sample question?'])
 
 def test_getImageInput_filepath():
     # compile image data as byte strings
@@ -46,8 +49,7 @@ def test_getImageInput_filepath():
     for i in range(len(image_list)):
         with open(image_list[i], 'rb') as f:
             files['input-image-%d'%(i)] = f.read()
-    r = requests.post(url=url, files=files, data={'test_number': 1})    
-    assert(r.status_code == 200)
+    r = requests.post(url=url, files=files, data={'test_number': 1})
     filepaths = json.loads(r.content)['data']
     assert(len(filepaths) == len(image_list))
     for i in range(len(filepaths)):
@@ -60,7 +62,7 @@ def test_getImageInput_nparray():
         with open(image_list[i], 'rb') as f:
             files['input-image-%d'%(i)] = f.read()
     r = requests.post(url=url, files=files, data={'test_number': 2})
-    assert(r.status_code == 200)
+    # get image pixel data
     images = json.loads(r.content)['data']
     assert(len(images) == len(image_list))
     for i in range(len(images)):
@@ -70,10 +72,15 @@ def test_getWrongImageInput():
     # compile image data as byte strings
     files = {}
     for i in range(len(image_list)):
+        # images in wrong order
         with open(image_list[len(image_list) - 1 - i], 'rb') as f:
             files['input-image-%d'%(i)] = f.read()
     r = requests.post(url=url, files=files, data={'test_number': 2})
-    assert(r.status_code == 500)
+    # get image pixel data
+    images = json.loads(r.content)['data']
+    assert(len(images) == len(image_list))
+    for i in range(len(images)):
+        assert(not np.array_equal(np.array(images[i], dtype=np.int16), cv2.imread(image_list[i])))
 
 def test_all():
     data_copy = data.copy()
@@ -83,4 +90,14 @@ def test_all():
         with open(image_list[i], 'rb') as f:
             files['input-image-%d'%(i)] = f.read()
     r = requests.post(url=url, files=files, data=data_copy)
-    assert(r.status_code == 200)
+    recv_data = json.loads(r.content)
+    text_array = recv_data['text']
+    image_paths = recv_data['filepaths']
+    images = recv_data['images']
+    assert(text_array == ['hello', 'world', 'Is this a sample question?'])
+    assert(len(images) == len(image_list))
+    for i in range(len(images)):
+        assert(np.array_equal(np.array(images[i], dtype=np.int16), cv2.imread(image_list[i])))
+    assert(len(image_paths) == len(image_list))
+    for i in range(len(image_paths)):
+        assert(np.array_equal(cv2.imread(image_paths[i]), cv2.imread(image_list[i])))
