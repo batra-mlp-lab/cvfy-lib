8ae03fdd3bceb322585bcdcb03778d0760f30549
Deepesh Pathak <deepshpathak@gmail.com>
2018-05-30 10:19:55 +0530

    feat(origami-api): add basic decorator for origami api functionalities.

--- origami/origami.py
+++ origami/origami.py
@@ -1,4 +1,4 @@
-from flask import Flask, request as user_req
+from flask import Flask, request as user_req, Response
 from flask_cors import CORS, cross_origin
 import magic
 from tornado.wsgi import WSGIContainer
@@ -32,6 +32,13 @@ class OrigamiInputs(OrigamiPipeline):
     def get_text_array(self):
         """
         Extract text input from the request form.
+
+        Returns:
+            text_inputs: Input text array provided by user in the request.
+
+        Raises:
+            InvalidRequestParameterGet: Not a valid parameter requested from
+                the users request to origami.
         """
         text_inputs = []
         i = 0
@@ -57,6 +64,15 @@ class OrigamiInputs(OrigamiPipeline):
             mode: mode in which you are expecting the result
                 file_path -> cache Image locally and return path
                 numpy_array ->
+
+        Returns:
+            ImageArr: array of Images either in the numpy array format or
+                the path of the image which is cached locally.
+
+        Raises:
+            InputHandlerException: Exception that the input provided by user in
+                the request is not Valid, which is some image is expected and
+                none provided.
         """
         image_inputs = []
         i = 0
@@ -80,8 +96,96 @@ class OrigamiInputs(OrigamiPipeline):
 
 
 class OrigamiOutputs(object):
+    """ Origami output functionalities
+    This class implements all the output functions for Origami.
+
+    Attributes:
+        response: response variable storing response to be sent to client
+            if API access is enabled using the provided decorator.
+    """
+    response = constants.DEFAULT_ORIGAMI_RESPONSE_TEMPLATE
+
     def __init__(self):
         pass
+
+    def _clear_response(self):
+        """
+        Clears the response variable to have the default template
+        response string
+
+        Returns:
+            response: string which was in self.response before clearing
+                it up.
+        """
+        response = self.response
+        self.response = constants.DEFAULT_ORIGAMI_RESPONSE_TEMPLATE
+        return response
+
+    def _request_origami_server(self):
+        pass
+
+    def _send_api_response(self):
+        pass
+
+    def origami_api(self, view_func):
+        """
+        Decorator to decorate the user defined main function to
+        send user an API response at the end of the request.
+
+        Args:
+            view_func: Function that this function wraps to make
+                things work.
+
+        Returns:
+            Wrapper fuction that calls the view_func to do its work
+            and then returns the response back to user.
+        """
+        def _wrapper():
+            view_func()
+            response = self._clear_response()
+            return response
+        return _wrapper
+
+    def send_text_array(self, data):
+        """
+        Send text data array to origami_server with the users socket ID
+
+        Args:
+            data: list or tuple of string to be sent.
+
+        Returns:
+            resp: Response text we got back from the origami server
+            corresponding to the request we made.
+
+        Raises:
+            MismatchTypeException: Type of the data provided to function is not
+                what we expected.
+        """
+        if not isinstance(data, (list, tuple)):
+            raise exceptions.MismatchTypeException(
+                "send_text_array can only accept an array or a tuple")
+
+        if not all(utils.check_if_string(element) for element in data):
+            raise exceptions.MismatchTypeException(
+                "send_text_array expects a list or tuple of string")
+
+        resp = None
+        try:
+            # Check if the socket-id is there is the request form.
+            socketId = user_req.form.get('socket-id', type=str)
+            payload = {
+                'socketId': socketId,
+                'data': data
+            }
+            resp = self._request_origami_server(payload)
+        except ValueError:
+            # TODO: Discuss the strucutre of API response payload.
+            payload = {
+                'data': data
+            }
+            resp = self._send_api_response(payload)
+        finally:
+            return resp
 
 
 class Origami(OrigamiInputs, OrigamiOutputs):
@@ -94,6 +198,10 @@ class Origami(OrigamiInputs, OrigamiOutputs):
         app_name: Application name.
         token: Origami token for the application.
         target: target of the interface CV/user deployment
+
+        server: Flask server for origami
+        cors: CORS for flask server running
+        mime: Mime type to deal with images for flask server
     """
 
     def __init__(self, app_name, token):
