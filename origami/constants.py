4614a3777e2853e9ff5c4e50d81d1a45e8779ce1
Deepesh Pathak <deepshpathak@gmail.com>
2018-06-01 13:10:27 +0530

    fix(lint): fix lint errors and add yapf formatter with pre-commit hook

--- origami/constants.py
+++ origami/constants.py
@@ -2,39 +2,31 @@ ORIGAMI_SERVER_INJECTION_PATH = "/event"
 ORIGAMI_SERVER_BASE_URL = "localhost:8000"
 DEFAULT_PORT = 9001
 
-
 LOCAL_TARGET_REGEXP = '^localhost|127\.0\.0\..|0\.0\.0\.0'
-
 
 HTTP_ENDPOINT = "http://"
 HTTPS_ENDPOINT = "https://"
-
 
 DEMO_DEPLOYMENT_TYPE = ["gh", "nongh"]
 LOCAL_TARGET = 'local'
 REMOTE_TARGET = 'remote'
 
-
 TOKEN_FORMAT = "nongh:0.0.0.0:1814173:5001:9001:0.0.0.0"
 
-
 ORIGAMI_DEFAULT_EVENT_ROUTE = "/event"
-
 
 INPUT_IMAGE_ARRAY_FILEPATH_MODE = "file_path"
 INPUT_IMAGE_ARRAY_NPARRAY_MODE = "numpy_array"
 
-
 REQUESTS_JSON_HEADERS = {'Content-Type': 'application/json'}
 
-
-DEFAULT_ORIGAMI_RESPONSE_TEMPLATE = [{"Copyright": """
+DEFAULT_ORIGAMI_RESPONSE_TEMPLATE = [{
+    "Copyright": """
 @CloudCV Origami Demo
-"""}]
-
+"""
+}]
 
 DEFAULT_DATA_TYPE_KEY = "data"
 TERMINAL_DATA_TYPE_KEY = "terminalData"
-
 
 REQUEST_SOCKET_ID_KEY = "socket-id"
